4393d11c0d501140af3ba13915ea92fd674ec063
Harender singh chauhan <harendersingh7896@gmail.com>
2022-11-13 20:38:58 +0530

    completed

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Project Work 2020
+                    </div>
+                    <div class="col-sm-6 text-right">
+                       Parker: One Stop Solution For Digital Parking.
+                    </div>
+                </div>
+            </div>
+        </footer>
